0bc92516855e9ba8d6384d098159087e1fc6b5b4
anastasiiamacbook <anastasia.shevchenko2154@gmail.com>
2025-10-16 19:43:09 +0300

    init commit

--- src/tests/ui/createAccount.spec.ts
+++ src/tests/ui/createAccount.spec.ts
@@ -1,5 +1,4 @@
 import {test} from "../fixtures/uiFixture";
-import {CreateAccountPage} from "@pages/createAccountPage";
 
 test.describe('Paydo account tests', () => {
 
@@ -10,7 +9,7 @@ test.describe('Paydo account tests', () => {
         });
 
         await test.step('2. Check elements on "Create a personal account" page', async () => {
-            const createAccountPage: CreateAccountPage = await landingPage.openCreateAccountPage();
+            const createAccountPage = await landingPage.openCreateAccountPage();
             await createAccountPage.shouldSeeCreateAccountForm("Create a personal account");
             await createAccountPage.shouldSeeTooltip("Our sales and other teams may contact you by this email, please make sure you are checking it from time to time.");
             await createAccountPage.shouldSeeSlider(["Individual IBANs", "Issuing physical and virtual cards", "Pay with PayDo checkout"]);
